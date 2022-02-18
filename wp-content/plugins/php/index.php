9e2de53d7c6adec576e3e38543596cd433aa5e4a
Abdou Aziz Alassane Maiga <abdoulaziz.maiga@odc.local>
2022-02-18 18:10:02 +0000

    commit2

--- wp-content/plugins/php/index.php
+++ wp-content/plugins/php/index.php
@@ -11,19 +11,19 @@ Author URI: http://w3guy.com
 function html_form_code() {
 	echo '<form action="' . esc_url( $_SERVER['REQUEST_URI'] ) . '" method="post">';
 	echo '<p>';
-	echo 'Your Name (required) <br/>';
+	echo 'Votre nom (required) <br/>';
 	echo '<input type="text" name="cf-name" pattern="[a-zA-Z0-9 ]+" value="' . ( isset( $_POST["cf-name"] ) ? esc_attr( $_POST["cf-name"] ) : '' ) . '" size="40" />';
 	echo '</p>';
 	echo '<p>';
-	echo 'Your Email (required) <br/>';
+	echo 'Votre Email (required) <br/>';
 	echo '<input type="email" name="cf-email" value="' . ( isset( $_POST["cf-email"] ) ? esc_attr( $_POST["cf-email"] ) : '' ) . '" size="40" />';
 	echo '</p>';
 	echo '<p>';
-	echo 'Subject (required) <br/>';
+	echo 'Sujet (required) <br/>';
 	echo '<input type="text" name="cf-subject" pattern="[a-zA-Z ]+" value="' . ( isset( $_POST["cf-subject"] ) ? esc_attr( $_POST["cf-subject"] ) : '' ) . '" size="40" />';
 	echo '</p>';
 	echo '<p>';
-	echo 'Your Message (required) <br/>';
+	echo 'Votre Message (required) <br/>';
 	echo '<textarea rows="10" cols="35" name="cf-message">' . ( isset( $_POST["cf-message"] ) ? esc_attr( $_POST["cf-message"] ) : '' ) . '</textarea>';
 	echo '</p>';
 	echo '<p><input type="submit" name="cf-submitted" value="Send"></p>';
